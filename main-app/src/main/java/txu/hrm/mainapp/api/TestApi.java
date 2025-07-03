872506113ee5baaccfdb80211a5f497544b86681
vovantung <vovantungdt123@gmail.com>
2025-07-03 16:36:38 +0700

    cm55

--- main-app/src/main/java/txu/hrm/mainapp/api/TestApi.java
+++ main-app/src/main/java/txu/hrm/mainapp/api/TestApi.java
@@ -3,12 +3,14 @@ package txu.hrm.mainapp.api;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import txu.common.exception.TxException;
 
 @CrossOrigin(origins = "*", allowCredentials = "false", maxAge = 86400, allowedHeaders = "*")
 @RestController
 public class TestApi {
     @GetMapping(value = "/test")
     public String test() {
-        return "test";
+        throw new TxException("test");
+//        return "test";
     }
 }
