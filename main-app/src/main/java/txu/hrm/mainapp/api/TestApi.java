afe49d06ee14dd1895eab95a4fc41d9e6d246777
vovantung <vovantungdt123@gmail.com>
2025-07-16 16:09:02 +0700

    cm96

--- main-app/src/main/java/txu/hrm/mainapp/api/TestApi.java
+++ main-app/src/main/java/txu/hrm/mainapp/api/TestApi.java
@@ -1,16 +1,18 @@
 package txu.hrm.mainapp.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import txu.common.exception.TxException;
+import txu.hrm.mainapp.dao.DtoTest;
+import txu.hrm.mainapp.dto.GetPostRequest;
 
 @CrossOrigin(origins = "*", allowCredentials = "false", maxAge = 86400, allowedHeaders = "*")
 @RestController
 public class TestApi {
-    @GetMapping(value = "/test")
-    public String test() {
+    @PostMapping(value = "/test")
+    public DtoTest test() {
 //        throw new TxException("test");
-        return "test";
+        DtoTest test = new DtoTest();
+        test.setTest("Hello World");
+        return test;
     }
 }
---
+++ main-app/src/main/java/txu/hrm/mainapp/dao/DtoTest.java
@@ -0,0 +1,10 @@
+package txu.hrm.mainapp.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DtoTest {
+    String test;
+}
